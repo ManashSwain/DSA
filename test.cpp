f4c0473ca6ad87e390ffc1de91f1f85ca8e620c0
ManashSwain <manash0802@gmail.com>
2024-12-03 17:26:01 +0530

    added test commands

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int b = 'a';
+    cout << "Hello world!" << endl ;
+    cout << b << endl;
+    cout << sizeof(b) << endl;
+    return 0 ;
+}
